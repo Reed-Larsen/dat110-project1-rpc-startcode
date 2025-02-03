2e552306f8c85102f91ccab5f9af9ccaf2ea92d8
Mikal Reed-Larsen <585124@stud.hvl.com>
2025-02-03 11:44:54 +0100

    Message metode som sier at message ikke kan inneholde mer enn 127 bytes.

--- src/main/java/no/hvl/dat110/messaging/Message.java
+++ src/main/java/no/hvl/dat110/messaging/Message.java
@@ -5,17 +5,19 @@ import no.hvl.dat110.TODO;
 public class Message {
 
 	// the up to 127 bytes of data (payload) that a message can hold
+	private static final int MAXSIZE = 127;
 	private byte[] data;
 
 	// construction a Message with the data provided
 	public Message(byte[] data) {
-		
-		// TODO - START
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.constructor("Message"));
-			
-		// TODO - END
+		if (data == null){
+			throw new IllegalArgumentException("Data cannot be null");
+
+		}
+		if (data.length > MAXSIZE){
+			throw new IllegalArgumentException("Data length cannot exceed" + MAXSIZE+ " bytes");
+		}
+		this.data = data;
 	}
 
 	public byte[] getData() {
