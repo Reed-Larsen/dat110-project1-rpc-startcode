fadace7d26df0433862ec829844f240dfa65cb94
585124 <mikal-44@hotmail.com>
2025-02-20 19:16:02 +0100

    MessageServer gjort

--- src/main/java/no/hvl/dat110/messaging/MessagingServer.java
+++ src/main/java/no/hvl/dat110/messaging/MessagingServer.java
@@ -2,6 +2,7 @@ package no.hvl.dat110.messaging;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import no.hvl.dat110.TODO;
 
@@ -28,14 +29,18 @@ public class MessagingServer {
 
 		MessageConnection connection = null;
 
-		// TODO - START
-		// accept TCP connection on welcome socket and create messaging connection to be returned
 
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
+		try {
+			// Vent på en klientforbindelse
+			Socket connectionSocket = welcomeSocket.accept();
+
+			// Opprett en MessageConnection for kommunikasjon
+			connection = new MessageConnection(connectionSocket);
+		} catch (IOException ex) {
+			System.out.println("Error accepting connection: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+
 		return connection;
 
 	}
